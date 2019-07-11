d5bc6074ea650e50c796a7e30505041957a93332
Winnguyen1511 <48133474+Winnguyen1511@users.noreply.github.com>
2019-07-11 10:38:41 +0700

    Add files via upload

---
+++ model/Account.java
@@ -0,0 +1,45 @@
+package com.example.model;
+
+import java.io.Serializable;
+
+public class Account implements Serializable {
+//    private int id;
+    private String username;
+    private String password;
+
+    public Account( String username, String password) {
+//        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Account() {
+//        id= 0;
+        username="";
+        password ="";
+    }
+
+//    public int getId() {
+//        return id;
+//    }
+
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
